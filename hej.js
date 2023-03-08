d19ea67462ac3f67371dfab3c73066a8d2313c56
Vitus Holm <vitus.kholm@gmail.com>
2023-03-08 17:22:34 +0100

    remove hej.js - can't remember why I saved it

--- hej.js
+++
@@ -1,8 +0,0 @@
-import { hashSync } from "bcrypt";
-
-for (let i = 5; i < 20; i++) {
-	let start = performance.now();
-	console.log(hashSync("password", i));
-	let end = performance.now();
-	console.log("Time: " + (end - start) + "ms (" + i + " rounds)");
-}
