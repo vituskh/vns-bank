e5bede8a10d0ed223b67593010c0c0dbba74f078
Vitus Holm <37027595+vituskh@users.noreply.github.com>
2023-03-18 12:48:09 +0100

    set NODE_ENV on tests

--- test/helper.js
+++ test/helper.js
@@ -3,10 +3,15 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 mongoose.set("strictQuery", false);
 import { setSaltRounds } from "../passwordManager.js";
 
+import config, { setVariable } from "../environment.js";
+setVariable("NODE_ENV", "test");
+
 let mongoServer;
 before(async function () {
 	setSaltRounds(1);
-
+	if (config.NODE_ENV != "test") {
+		throw new Error("NODE_ENV is not set to test");
+	}
 	mongoServer = await MongoMemoryServer.create();
 	const mongoUri = await mongoServer.getUri();
 	await mongoose.connect(mongoUri, {
